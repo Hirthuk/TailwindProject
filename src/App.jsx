ebd8e91b4aa69f3d219ffbe755007c730e93c0d7
Hirthuk <sharankumarp02@gmail.com>
2025-03-10 09:40:50 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-center rounded-md border-l-gray-950  bg-white shadow-lg">
+      Hello world!
+    </h1>
+  )
+}
