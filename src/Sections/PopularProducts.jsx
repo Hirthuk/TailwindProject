bf6e72089a23e96f40b2df85b9984e6680fc9f43
Hirthuk <sharankumarp02@gmail.com>
2025-03-15 09:13:56 +0530

    Super product started

--- src/Sections/PopularProducts.jsx
+++ src/Sections/PopularProducts.jsx
@@ -3,18 +3,18 @@ import { star } from "../assets/icons"
 
 const PopularProducts = () => {
   return (
-   <section className="flex flex-col w-full h-full gap-6 justify-start flex-wrap">
+   <section className="flex flex-col w-full h-full gap-6  flex-wrap justify-center">
     <div className="flex flex-col gap-8 text-start relative top-0 mb-10">
       <h1 className="font-montserrat text-4xl font-bold max-lg:text-3xl">Our <span className="font-palanquin text-coral-red">Popular</span> Products</h1>
       <p className="flex flex-col text-2xl max-sm:text-1xl max-md:text-2xl max-lg:text-2xl max-lg:max-w-lg text-slate-500 max-lg:-mt-2">Experience top-notch quality and style with our sought-after selections.Discover a world of comfort, design, and value</p>
     </div>
-    <div className="flex flex-row justify-start text-start gap-11 flex-wrap">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5">
       {products.map((item) => (
-        <div>
+        <div className="w-full">
           <img src={item.imgURL} alt="ImageURL"/>
-          <h2 className="flex flex-col flex-wrap mt-10 gap-2">
+          <h2 className="flex flex-col flex-wrap mt-10 gap-2 text-2xl sm:text-sm md:text-xl">
            <span className="flex flex-row gap-3"> <img src={star} alt="Star"/><h2 className="text-[1.5rem] text-slate-500 font-montserrat">(4.5)</h2></span>
-           <h2 className="text-2xl font-montserrat font-bold flex flex-col gap-3">
+           <h2 className="text-2xl text-start font-montserrat font-bold flex flex-col gap-3">
             <span>{item.name} </span>
             <span className="text-coral-red">{item.price}</span>
             </h2>
--- src/Sections/SuperQuality.jsx
+++ src/Sections/SuperQuality.jsx
@@ -1,9 +1,19 @@
+import { shoe8 } from "../assets/images"
 
 const SuperQuality = () => {
   return (
-    <div>
-      SuperQuality
-    </div>
+    <section id="about-us" className="flex flex-row w-full gap-[10%] align-top">
+      <div className="w-[50%] flex flex-col flex-wrap justify-evenly">
+        <h1 className="font-montserrat text-4xl font-bold -mt-7">We Provide You Super Quality Shoes</h1>
+        <p>Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
+          <br />
+          Our dedication to detail and excellence ensures your satisfaction</p>
+          <button className="bg-sky-500">View details</button>
+      </div>
+      <div className="w-[50%]">
+        <img src={shoe8} alt="" srcset="" />
+      </div>
+    </section>
   )
 }
 
